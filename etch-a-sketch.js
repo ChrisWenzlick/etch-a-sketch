1eb30a2cf6ee88bb0e3d512de97e1a60f6dfca4f
ChrisWenzlick <cwenzlick01@gmail.com>
2024-04-03 20:07:54 -0500

    Add randomized color drawing when user clicks and drags in the grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("#canvas");
 
 let gridSize = 16;
+let clickToColor = true;
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
 
 
 InitializeGrid();
@@ -17,7 +20,38 @@ function InitializeGrid() {
         for(let cell = 0; cell < gridSize; cell++) {
             const gridCell = document.createElement("div");
             gridCell.className = "grid-cell";
+
+            gridCell.addEventListener('mouseover', () => {
+                ColorWithHover(gridCell);
+            });
+
+            gridCell.addEventListener('dragenter', () => {
+                ColorWithDrag(gridCell);
+            });
+
             gridRow.appendChild(gridCell);
         }
     }
+}
+
+function ColorWithHover(gridCell) {
+    if(!clickToColor) {
+        gridCell.style.backgroundColor = GetColor();
+    }
+}
+
+function ColorWithDrag(gridCell) {
+    if(clickToColor) {
+        gridCell.style.backgroundColor = GetColor();
+    }
+}
+
+function GetColor() {
+    let newHexColor = "#";
+    for(let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * hexCharacters.length);
+        newHexColor += hexCharacters[randomIndex];
+    }
+
+    return newHexColor;
 }
